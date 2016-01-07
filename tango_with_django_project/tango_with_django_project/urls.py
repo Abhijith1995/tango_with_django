edb0404a15e6e402dbec893513a4bfd32a20a719
Abhijith Ramalingam <ajibith1995@gmail.com>
2016-01-07 17:47:34 +0400

    Added view

--- tango_with_django_project/tango_with_django_project/urls.py
+++ tango_with_django_project/tango_with_django_project/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf.urls import patterns, url 
+from rango import views
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 ]
+urlpatterns = patterns('',
+        url(r'^$', views.index, name='index'))
